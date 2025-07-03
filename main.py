2771d21ab117aaaa6029d405fd3e08b79b37d758
IrfanAliMN <76953877+IrfanAliMN@users.noreply.github.com>
2025-07-04 00:30:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+class BankAccount:
+  def __init__(self,name,initial_balance):
+    self.name=name
+    self.balance=initial_balance
+  
+  def deposit(self, amount):
+    self.balance+=amount
+  def withdraw(self, amount):
+    self.balance-=amount
+  def check_balance(self):
+    print('Balance Money'+ self.balance)
+
+
+b1= BankAccount('irfan',250)
+print(b1.name)
+print(b1.balance)
+
+b1.deposit(750)
+b1.check_balance()
+b1.withdraw(500)
+b1.check_balance
